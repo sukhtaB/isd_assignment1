70c57502fe6ef8efc0da237b3e06b1b3e9216d14
swarya <swarya@academic.rrc.ca>
2024-09-14 22:57:08 -0500

    Completed unit tests for Client class

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,8 @@
 """
 Description: Unit tests for the Client class.
 Author: ACE Faculty
-Modified by: {Student Name}
-Date: {Date}
-Usage: To execute all tests in the terminal execute 
-the following command:
-    python -m unittest tests/test_client.py
+Modified by: Sukhtab Singh Warya
+Date: 10/09/2024
 """
 
 import unittest
