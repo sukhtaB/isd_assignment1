cbeb60b9f6efa009514cf7f5cf3e6aa5224087dc
swarya <swarya@academic.rrc.ca>
2024-10-06 15:22:38 -0500

    update the chequing account file

--- bank_account/chequing_account.py
+++ bank_account/chequing_account.py
@@ -1,1 +1,5 @@
-""
+"""
+Description: This file defines the ChequingAccount class, which represents a chequing account with basic operations such as deposit and withdrawal.
+Author: Sukhtab Singh Warya
+Date: 06/10/2024
+"""
